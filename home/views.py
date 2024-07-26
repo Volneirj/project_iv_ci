1f9383919607e7cf15569e804aadb3f576a3a025
Volnei Resena Junior <volnei.resena.j@hotmail.com>
2024-07-26 17:44:55 +0100

    Add content to home page

--- home/views.py
+++ home/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from books.models import Book
+from django.db.models import Count
 
 def home(request):
-    return render(request, 'home/home.html')
+    # Annotate each book with the count of related issues
+    top_books = Book.objects.annotate(issue_count=Count('title')).order_by('-issue_count')[:3]
+
+    context = {
+        'top_books': top_books,
+    }
+    return render(request, 'home/home.html', context)
